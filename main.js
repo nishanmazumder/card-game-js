08b8e339df7ba085f97daad84a73d82a58d4371b
nishanmazumder <arosh019@gmail.com>
2022-12-24 14:34:44 +0600

    flip 3d done

---
+++ main.js
@@ -0,0 +1,8 @@
+const cardObjEle = [
+    {id:1, path:'/img/card-kingH.png'},
+    {id:2, path:'/img/card-jackF.png'},
+    {id:3, path:'/img/card-queenD.png'},
+    {id:4, path:'/img/card-acespd.png'}
+]
+
+const cardImgBack = "/img/card-back.png"
